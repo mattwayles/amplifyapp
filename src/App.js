def2e1d2de79d960dfd6c70deb10909fa81c1e41
Matt W <mattwayles@gmail.com>
2021-12-29 16:16:10 -0700

    Added notes api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Authenticator } from "@aws-amplify/ui-react";
 import React from "react";
 import '@aws-amplify/ui-react/styles.css'
+import NotesView from "./NotesView";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
             <h1>Hello, {user.username}</h1>
             <p>You are now authenticated.</p>
             <button onClick={signOut}>Sign Out</button>
+            <NotesView />
           </div>)
       }
     </Authenticator>
